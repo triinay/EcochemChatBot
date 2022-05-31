9ce41050ea37a4229a3af5c7be01f2847f6296ef
Арина Цыганкова <triinay15@gmail.com>
2022-05-31 13:37:31 +0300

    connection

---
+++ EcoChemBotVK/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using VkBotFramework;
+using VkBotFramework.Models;
+using VkNet.Model.RequestParams;
+
+namespace EcoChemBotVK
+{
+    internal class Program
+    {
+        private static string AccessToken = "c4db624efe67503f7b14d139a4d80a48b928b27511387a972461b7b7391c3e8125d7dce1098b6c8a4f8cd";
+        private static string GroupURL = "https://vk.com/club213632604";
+        private static VkBot _bot;
+
+        static void Main(string[] args)
+        {
+            _bot = new VkBot(AccessToken, GroupURL);
+            _bot.OnMessageReceived += BotOnMessageReceived;
+            _bot.Start();
+            Console.ReadLine();
+        }
+
+        private static void BotOnMessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            var chatId = e.Message.PeerId;
+            _bot.Api.Messages.Send(new MessagesSendParams()
+            {
+                Message = "Hello",
+                PeerId = chatId,
+                RandomId = Environment.TickCount
+            });
+        }
+    }
+}
