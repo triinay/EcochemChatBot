dc965e0b0c6440e72074b3975cd442810b025e5c
obendevet <nellisosenka@gmail.com>
2022-06-07 15:46:31 +0300

    reaction on user's messages

--- EcoChemBotVK/Program.cs
+++ EcoChemBotVK/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using VkBotFramework;
-using VkBotFramework.Models;
 using VkNet;
 using VkNet.Model.RequestParams;
 using VkNet.Model.Keyboard;
@@ -18,99 +14,55 @@ namespace EcoChemBotVK
 {
     internal class Program
     {
-        //private static string AccessToken = "c4db624efe67503f7b14d139a4d80a48b928b27511387a972461b7b7391c3e8125d7dce1098b6c8a4f8cd";
-        //private static string GroupURL = "https://vk.com/club213632604";
-        //private static VkBot _bot;
-
         static void Main(string[] args)
         {
-
-            //_bot = new VkBot(AccessToken, GroupURL);
-
-            //_bot.OnMessageReceived += BotOnMessageReceived;
-
-            //_bot.Start();
-
-            //Console.ReadLine();
             var api = new VkApi();
             api.Authorize(new ApiAuthParams
             {
-                AccessToken = "c4db624efe67503f7b14d139a4d80a48b928b27511387a972461b7b7391c3e8125d7dce1098b6c8a4f8cd"});
-            var dialogs = api.Messages.GetConversations(new GetConversationsParams() 
-            {
-                Filter = GetConversationFilter.Unread
+                AccessToken = "c4db624efe67503f7b14d139a4d80a48b928b27511387a972461b7b7391c3e8125d7dce1098b6c8a4f8cd"
             });
-            MessageKeyboard keyboard = DrawMainKeyboard();
-            long peer_id = dialogs.Items[0].Conversation.Peer.Id; 
+
             string bot_answer = "Здравствуйте! Вас приветствует виртуальный помощник компании Экохим.";
-            api.Messages.Send(new MessagesSendParams()
-            {
-                PeerId = peer_id,
-                Message = bot_answer,
-                RandomId = new Random().Next(minValue: 0, maxValue: 10000), 
-                Keyboard = keyboard 
-            });
+
+            VKMessageManager manager = new VKMessageManager();
+
+            manager.OnNewMessage += (message, sender) => {
+                MessageKeyboard keyboard = DrawMainKeyboard();
+
+                api.Messages.Send(new MessagesSendParams()
+                {
+                    PeerId = sender.Id,
+                    Message = bot_answer,
+                    RandomId = new Random().Next(minValue: 0, maxValue: 10000),
+                    Keyboard = keyboard
+                });
+            };
+
+            manager.StartMessagesHandling();
         }
 
-        private static void BotOnMessageReceived(object sender, VkBotFramework.Models.MessageReceivedEventArgs e)
-        {
-            var chatId = e.Message.PeerId;
-
-            MessageKeyboard keyboard = DrawMainKeyboard();
-
-            Console.WriteLine(CreateLinkButton()[0][0].Action.Type.ToString());
-
-            //_bot.Api.Messages.Send(new MessagesSendParams()
-            {
-                //Message = "Здравствуйте! Вас приветствует виртуальный помощник компании Экохим.",
-                //PeerId = chatId,
-                //RandomId = Environment.TickCount,
-                //Keyboard = keyboard
-                //});
-            }
-        }
 
         private static MessageKeyboard DrawMainKeyboard()
         {
-
-            var keyboard = new KeyboardBuilder();
-            //keyboard.AddButton("Какой герметик мне подойдёт?", "");
-            //keyboard.AddLine();
-            //keyboard.AddButton("Инструкция по применению", "");
-            //keyboard.AddLine();
-            //keyboard.AddButton("Заказать звонок", "");
-            //keyboard.AddLine();
-            //keyboard.AddButton("Рассчитать стоимость доставки", "");
-            //keyboard.AddLine();
-            //keyboard.AddButton("Найти тех. характеристики герметика", "");
-            //keyboard.AddLine();
-            //keyboard.AddButton("Получить скидку", "");
-            //keyboard.AddLine();
-            //keyboard.AddButton("Оставить отзыв", "");
-            MessageKeyboard keyboard_hui = keyboard.Build();
-            keyboard_hui.Buttons = CreateLinkButton();
-
-
-            return keyboard_hui;
+            var keyboardBuilder = new KeyboardBuilder();
+            MessageKeyboard keyboard = keyboardBuilder.Build();
+            keyboard.Buttons = CreateLinkButton();
+            return keyboard;
         }
+
 
         private static List<List<MessageKeyboardButton>> CreateLinkButton()
         {
-
             using var sw = new StreamReader("../../Data/settings.json", Encoding.UTF8);
             using var jsonReader = new JsonTextReader(sw);
             var serializer = new JsonSerializer()
             {
                 TypeNameHandling = TypeNameHandling.Auto
             };
-            
-            List<List<MessageKeyboardButton>> linkButton =  serializer.Deserialize<List<List<MessageKeyboardButton>>>(jsonReader);
-            Console.WriteLine(linkButton[0][0].Action.Link);
 
-            return linkButton;
+            List<List<MessageKeyboardButton>> buttons = serializer.Deserialize<List<List<MessageKeyboardButton>>>(jsonReader);
 
+            return buttons;
         }
-
-
     }
 }
---
+++ EcoChemBotVK/VKMessageManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using VkNet;
+using VkNet.Model.RequestParams;
+using VkNet.Model;
+using VkNet.Enums.Filters;
+using System.Threading;
+using System.Linq;
+
+namespace EcoChemBotVK
+{
+    class VKMessageManager
+    {
+        private VkApi _api = new VkApi();
+        private ulong ts;
+        private ulong? pts;
+        public event Action<Message, User> OnNewMessage;
+
+        public VKMessageManager()
+        { 
+            _api.Authorize(new ApiAuthParams()
+            {
+                AccessToken = "c4db624efe67503f7b14d139a4d80a48b928b27511387a972461b7b7391c3e8125d7dce1098b6c8a4f8cd"
+            });
+        }
+
+
+        public void StartMessagesHandling()
+        {
+            LongPollServerResponse longPoolServerResponse = _api.Messages.GetLongPollServer(needPts: true);
+            ts = Convert.ToUInt64(longPoolServerResponse.Ts);
+            pts = longPoolServerResponse.Pts;
+
+            new Thread(LongPollEventLoop).Start();
+        }
+
+
+        public void LongPollEventLoop()
+        {
+            while (true)
+            {
+                LongPollHistoryResponse longPollResponse = _api.Messages.GetLongPollHistory(new MessagesGetLongPollHistoryParams()
+                {
+                    Ts = ts,
+                    Pts = pts
+                });
+
+                pts = longPollResponse.NewPts;
+
+                for (int i = 0; i < longPollResponse.Messages.Count; i++)
+                {
+                    switch (longPollResponse.History[i][0])
+                    {
+                        //Код 4 - новое сообщение
+                        case 4 :
+                            OnNewMessage?.Invoke(
+                                longPollResponse.Messages[i],
+                                longPollResponse.Profiles
+                                 .Where(u => u.Id == longPollResponse.Messages[i].FromId)
+                                 .FirstOrDefault()
+                            );
+                            break;
+
+                        case 12:
+                            OnNewMessage?.Invoke(
+                                longPollResponse.Messages[i],
+                                longPollResponse.Profiles
+                                 .Where(u => u.Id == longPollResponse.Messages[i].FromId)
+                                 .FirstOrDefault()
+                            );
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
